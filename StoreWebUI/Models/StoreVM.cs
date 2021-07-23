0eded2ec810a5097ba962d5bfb4b2b0a10f53185
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-07-23 12:09:41 -0400

    Created StoreVM model

---
+++ StoreWebUI/Models/StoreVM.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using StoreModels;
+
+namespace StoreWebUI.Models
+{
+    public class StoreVM
+    {
+        [Required]
+        public int ID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Address { get; set; }
+        [Required]
+        public List<LineItem> Inventory { get; set; }
+        public List<Order> Orders { get; set; }
+
+        public StoreVM()
+        { }
+
+        public StoreVM(StoreFront p_store)
+        {
+            ID = p_store.ID;
+            Name = p_store.Name;
+            Address = p_store.Address;
+            Inventory = p_store.Inventory;
+            Orders = p_store.Orders;
+        }
+    }
+}
