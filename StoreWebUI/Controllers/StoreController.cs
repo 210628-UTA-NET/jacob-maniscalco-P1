78dfbf41b78cba789736335c8013b78ca95d4743
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-07-26 16:07:58 -0400

    Created AddInventory action to the customer controller to replenish store inventory

--- StoreWebUI/Controllers/StoreController.cs
+++ StoreWebUI/Controllers/StoreController.cs
@@ -27,5 +27,29 @@ namespace StoreWebUI.Controllers
         {
             return View(new StoreWebUI.Models.StoreVM(_storeBL.GetStoreFrontAll(p_StoreFrontID)));
         }
+
+        public IActionResult AddInventory(int p_StoreID, int p_ItemID)
+        {
+            return View(new StoreWebUI.Models.LineItemVM(_storeBL.GetLineItem(p_StoreID, p_ItemID)));
+        }
+
+        public IActionResult AddQuantity(StoreWebUI.Models.LineItemVM p_item)
+        {
+            Console.WriteLine("\n\nQuantity: "+ p_item.Quantity);
+            Console.WriteLine("\n\nID: " + p_item.ID);
+            StoreModels.LineItem item = new StoreModels.LineItem()
+            {
+                ID = p_item.ID,
+                StoreFrontID = p_item.StoreFrontID,
+                Product = p_item.Product,
+                Quantity = p_item.Quantity
+            };
+
+           StoreModels.LineItem updatedItem = _storeBL.updateInventory(item.StoreFrontID, item.ID, item.Quantity);
+
+           return RedirectToAction("Index");
+
+        }
     }
 }
+
