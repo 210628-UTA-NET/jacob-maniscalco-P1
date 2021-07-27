a445dadc2ce780997488d8ae6b7483c90bc6e5f9
Jacob Maniscalco <jmaniscalco9@icloud.com>
2021-07-27 19:26:08 -0400

    Added an Order ID field to order item

--- StoreModels/OrderItem.cs
+++ StoreModels/OrderItem.cs
@@ -4,8 +4,10 @@ namespace StoreModels
     public class OrderItem
     {
         public int ID { get; set; }
+        public int OrderID { get; set; }
         public Product Product { get; set; }
         public int Quantity { get; set; }
+        
         public OrderItem()
         {}
 
